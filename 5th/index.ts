35e72e101952ef052e5186e31024da5523064940
drafolin <derg@drafolin.ch>
2023-12-05 07:39:38 +0100

    step 1

--- 5th/index.ts
+++ 5th/index.ts
@@ -1,7 +1,7 @@
 import * as fs from "fs";
 
-//const data = fs.readFileSync(__dirname + "/data.txt", "utf-8").split("\n");
-const data = `seeds: 79 14 55 13
+const data = fs.readFileSync(__dirname + "/data.txt", "utf-8").split("\n\n");
+/*const data = `seeds: 79 14 55 13
 
 seed-to-soil map:
 50 98 2
@@ -34,6 +34,47 @@ temperature-to-humidity map:
 humidity-to-location map:
 60 56 37
 56 93 4
-`.split("\n");
-data.pop();
-//find the lowest location number that corresponds to any of the initial seeds.;
+`.split("\n\n");*/
+//destination source length
+
+console.log(data);
+
+const seeds = data[0].split(" ").slice(1).map(v => parseInt(v));
+seeds;
+const maps = data.slice(1).map(v => v.trim().split("\n").slice(1).map(v => v.split(" ").map(v => parseInt(v))));
+let mapped = seeds;
+for (let map of maps) {
+	const currentMap = new Map<string, number>();
+
+	for (let line of map) {
+		let destination = line[0];
+		let source = line[1];
+		currentMap.set(`${source} ${source + line[2] - 1}`, destination);
+	}
+
+	let temp: number[] = [];
+	for (let seed of mapped) {
+		let ok = false;
+		[...currentMap.keys()].forEach(v => {
+			if (ok)
+				return;
+			let nums = v.split(" ").map(v => parseInt(v));
+			if (nums[0] <= seed && seed <= nums[1]) {
+				temp.push(currentMap.get(v)! + (seed - nums[0]));
+				ok = true;
+			}
+		});
+		if (!ok)
+			temp.push(seed);
+	}
+	mapped = temp;
+}
+
+
+let min = Infinity;
+for (let location of mapped) {
+	if (location < min)
+		min = location;
+}
+
+min;
