26adb7b07c87f3aae648de13f5b13fc279cd8082
drafolin <derg@drafolin.ch>
2023-12-06 15:08:31 +0100

    step to 2

--- 6th/index.ts
+++ 6th/index.ts
@@ -4,31 +4,21 @@ import * as fs from "fs";
 	`Time:      7  15   30
 Distance:  9  40  200`.split("\n");*/
 const dataTxt = fs.readFileSync(__dirname + "/data.txt", "utf-8").split("\n");
-const times = dataTxt[0].split(" ").filter(v => v !== "").slice(1).map(v => parseInt(v));
-const distances = dataTxt[1].split(" ").filter(v => v !== "").slice(1).map(v => parseInt(v));
+const times = parseInt(dataTxt[0].split(" ").filter(v => v !== "").slice(1).join(""));
+const distances = parseInt(dataTxt[1].split(" ").filter(v => v !== "").slice(1).join(""));
 
-const data: { time: number; distance: number; }[] = [];
+const data: { time: number; distance: number; } = { time: times, distance: distances };
 
+let count = 0;
+for (let i = 0; i <= data.time; i++) {
+	// i = temps appuyé = speed mm/ms
+	const runTime = data.time - i;
+	const spd = i;
+	const distance = spd * runTime;
 
-for (let i = 0; i < times.length; ++i) {
-	data.push({ time: times[i], distance: distances[i] });
-}
-
-let total = 1;
-
-for (let race of data) {
-	let count = 0;
-	for (let i = 0; i <= race.time; i++) {
-		// i = temps appuyé = speed mm/ms
-		const runTime = race.time - i;
-		const spd = i;
-		const distance = spd * runTime;
-
-		if (distance > race.distance) {
-			++count;
-		}
+	if (distance > data.distance) {
+		++count;
 	}
-	total *= count;
 }
 
-total;
+count;
