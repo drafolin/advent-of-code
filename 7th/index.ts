6533a391d97a09b2190c6f64f1124886b19cc3b8
drafolin <derg@drafolin.ch>
2023-12-07 08:55:32 +0100

    day setup

---
+++ 7th/index.ts
@@ -0,0 +1,9 @@
+import * as fs from "fs";
+
+//const data = fs.readFileSync(__dirname + "/data.txt", "utf-8").split("\n");
+const data = `32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483
+`.split("\n"); 
