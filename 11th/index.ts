ad86a0d53c210871dbded74952f20fba15d1cd8d
drafolin <derg@drafolin.ch>
2023-12-11 10:38:31 +0100

    step 2

--- 11th/index.ts
+++ 11th/index.ts
@@ -20,24 +20,28 @@ const dataTmp = dataTxt.map(v => v.split(""));
 let dataRowTmp: string[][] = [];
 for (let line of dataTmp) {
 	if (!line.includes("#"))
+		dataRowTmp.push(line.map(v => v.replace(".", "-")));
+	else
 		dataRowTmp.push(line);
-	dataRowTmp.push(line);
 }
 let data: string[][] = [];
 for (let i in dataRowTmp[0]) {
-	if (dataRowTmp.every(v => v[i] === "."))
+	if (dataRowTmp.every(v => v[i] === "." || v[i] === "-"))
+		dataRowTmp.forEach((v, j) => {
+			if (!data[j])
+				data[j] = [];
+			if (dataRowTmp[j][i] === ".")
+				data[j].push("|");
+			else
+				data[j].push("+");
+		});
+	else
 		dataRowTmp.forEach((v, j) => {
 			if (!data[j])
 				data[j] = [];
 			data[j].push(v[i]);
 		});
-	dataRowTmp.forEach((v, j) => {
-		if (!data[j])
-			data[j] = [];
-		data[j].push(v[i]);
-	});
 }
-
 
 let galaxies: Point[] = [];
 
@@ -48,10 +52,39 @@ data.forEach((v, i) => {
 	});
 });
 
+console.log(data.map(v => v.join("")).join("\n"));
+
+const EXPANSION_FACTOR = 1_000_000;
+
 let sum = 0;
 for (let i = 0; i < galaxies.length - 1; ++i) {
 	for (let j = i + 1; j < galaxies.length; ++j) {
-		sum += Math.abs(galaxies[i].x - galaxies[j].x) + Math.abs(galaxies[i].y - galaxies[j].y);
+		const galaxy1 = galaxies[i];
+		const galaxy2 = galaxies[j];
+
+		const minX = Math.min(galaxy1.x, galaxy2.x);
+		const maxX = Math.max(galaxy1.x, galaxy2.x);
+		const minY = Math.min(galaxy1.y, galaxy2.y);
+		const maxY = Math.max(galaxy1.y, galaxy2.y);
+
+		let totalX = 0;
+		let totalY = 0;
+
+		for (let x = minX + 1; x <= maxX; ++x) {
+			if (data[minY][x] === "|")
+				totalX += EXPANSION_FACTOR;
+			else
+				totalX += 1;
+		}
+
+		for (let y = minY + 1; y <= maxY; ++y) {
+			if (data[y][maxX] === "-")
+				totalY += EXPANSION_FACTOR;
+			else
+				totalY += 1;
+		}
+
+		sum += totalX + totalY;
 	}
 }
 
