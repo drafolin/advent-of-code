e8fe8837f4ecced2dd115c9c8c927807263c9d00
drafolin <derg@drafolin.ch>
2023-12-11 09:00:39 +0100

    galaxy expansion

--- 11th/index.ts
+++ 11th/index.ts
@@ -13,3 +13,26 @@ const dataTxt = `...#......
 `.split("\n");
 //const dataTxt = readFileSync(__dirname + "/data.txt", "utf-8").split("\n");
 
+
+dataTxt.pop();
+const dataTmp = dataTxt.map(v => v.split(""));
+let dataRowTmp: string[][] = [];
+for (let line of dataTmp) {
+	if (!line.includes("#"))
+		dataRowTmp.push(line);
+	dataRowTmp.push(line);
+}
+let data: string[][] = [];
+for (let i in dataRowTmp[0]) {
+	if (dataRowTmp.every(v => v[i] === "."))
+		dataRowTmp.forEach((v, j) => {
+			if (!data[j])
+				data[j] = [];
+			data[j].push(v[i]);
+		});
+	dataRowTmp.forEach((v, j) => {
+		if (!data[j])
+			data[j] = [];
+		data[j].push(v[i]);
+	});
+}
