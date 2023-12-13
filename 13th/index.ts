8b74adb812e1f7651bd0542d13a34c06a2dafd46
drafolin <derg@drafolin.ch>
2023-12-13 10:31:06 +0100

    step 2

--- 13th/index.ts
+++ 13th/index.ts
@@ -19,24 +19,40 @@ const dataTxt = (testing ? `#.##..##.
 #....#..#
 ` : readFileSync(__dirname + "/data.txt", "utf-8")).trim().split("\n\n");
 
+const countDiff = (s1: string, s2: string): number => {
+	let count = 0;
+	let a1 = s1.split("");
+	let a2 = s2.split("");
+
+	for (let i in a1) {
+		if (a1[i] !== a2[i])
+			++count;
+	}
+
+	console.log(a1, a2, count);
+
+	return count;
+};
+
 const findMirror = (arr: string[]): number | null => {
 	for (let lineI = 1; lineI < arr.length; ++lineI) {
-		if (arr[lineI - 1] === arr[lineI]) {
-			let isValid = true;
-			if (lineI > arr.length / 2) {
-				for (let i = 0; i < arr.length - lineI; ++i) {
-					if (arr[lineI + i] !== arr[lineI - i - 1])
-						isValid = false;
-				}
-			} else {
-				for (let i = 0; i < lineI; ++i) {
-					if (arr[lineI + i] !== arr[lineI - i - 1])
-						isValid = false;
-				}
+		let diffCount = 0;
+		if (lineI > arr.length / 2) {
+			for (let i = 0; i < arr.length - lineI; ++i) {
+				diffCount += countDiff(arr[lineI - i - 1], arr[lineI + i]);
+				if (diffCount > 1)
+					break;
 			}
-			if (isValid)
-				return lineI;
+		} else {
+			for (let i = 0; i < lineI; ++i) {
+				diffCount += countDiff(arr[lineI - i - 1], arr[lineI + i]);
+				if (diffCount > 1)
+					break;
+			}
 		}
+		console.log(lineI, diffCount);
+		if (diffCount === 1)
+			return lineI;
 	}
 	return null;
 };
