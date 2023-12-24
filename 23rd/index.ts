76fe73707012fc92da39a557bb8fb21327084ab4
drafolin <derg@drafolin.ch>
2023-12-24 10:35:54 +0100

    step 2

--- 23rd/index.ts
+++ 23rd/index.ts
@@ -43,79 +43,91 @@ while (dataTxt[dataTxt.length - 1][endColumn] === "#")
 
 type Point = { x: number, y: number; };
 
-class Solver {
-	pos: Point;
-	/**
-	 * Not including current pos.
-	 * Contains the solver path, in string formatted "x,y"
-	*/
-	path: string[];
-	constructor(pos: Point, path: string[]) {
-		this.pos = pos;
-		this.path = path;
+const deltas: Point[] = [
+	{ x: 0, y: -1 },
+	{ x: 0, y: 1 },
+	{ x: -1, y: 0 },
+	{ x: 1, y: 0 }
+];
+
+const directions: Record<string, Point[]> = {
+	'^': [{ x: 0, y: -1 }],
+	'v': [{ x: 0, y: 1 }],
+	'<': [{ x: -1, y: 0 }],
+	'>': [{ x: 1, y: 0 }],
+	'.': deltas,
+};
+
+
+function getGraph() {
+	const points = new Map<string, string>(dataTxt.flatMap((row, rowI) => row.map((char, colI) => [`${colI},${rowI}`, char])));
+
+	const graph = Object.fromEntries(
+		[...points].map((point) => [
+			point[0],
+			{} as Record<string, number>,
+		])
+	);
+
+	for (const [sx, sy] of [...points.keys()].map(s => s.split(","))) {
+		const stack: [number, number, number][] = [[0, +sy, +sx]];
+		const seen = new Set<string>([`${sx},${sy}`]);
+
+		while (stack.length) {
+			const [n, y, x] = stack.shift()!;
+
+			if (n && points.has(`${x},${y}`)) {
+				graph[`${sx},${sy}`][`${x},${y}`] = n;
+				continue;
+			}
+
+			const tile = points.get(`${x},${y}`)!;
+
+			if (tile === '#') continue;
+
+			for (const { x: dx, y: dy } of (deltas) as Point[]) {
+				const nx = x + dx;
+				const ny = y + dy;
+
+				if (ny < 0 || ny >= dataTxt.length || nx < 0 || ny >= dataTxt[0].length)
+					continue;
+
+				const newTile = dataTxt.at(ny)?.at(nx);
+
+				if (!newTile || newTile === '#') continue;
+
+				if (seen.has(`${nx},${ny}`)) continue;
+
+				stack.push([n + 1, ny, nx]);
+				seen.add(`${nx}, ${ny}`);
+			}
+		}
 	}
 
-	tryMove = () => {
-		let res: Solver[] = [];
-		[
-			{ x: this.pos.x - 1, y: this.pos.y },
-			{ x: this.pos.x + 1, y: this.pos.y },
-			{ x: this.pos.x, y: this.pos.y - 1 },
-			{ x: this.pos.x, y: this.pos.y + 1 }
-		]
-			.filter(v => !this.path.includes(`${v.x},${v.y}`))
-			.forEach(v => {
-				if (map.has(`${v.x},${v.y}`)) {
-					switch (map.get(`${v.x},${v.y}`)) {
-						case "#": return;
-						case ">":
-							if (this.pos.x >= v.x)
-								return;
-							break;
-						case "^":
-							if (this.pos.y <= v.y)
-								return;
-							break;
-						case "<":
-							if (this.pos.x <= v.x)
-								return;
-							break;
-						case "v":
-							if (this.pos.y >= v.y)
-								return;
-							break;
-					}
-					res.push(new Solver(v, [...this.path, `${this.pos.x},${this.pos.y}`]));
-				}
-			});
-		return res;
+	return graph;
+}
+
+const seen = new Set<string>();
+const graph = getGraph();
+const dfs = (point: Point, depth: number) => {
+	if (point.x === endColumn && point.y === dataTxt.length - 1)
+		return 0;
+
+
+	let max = -Infinity;
+
+	seen.add(`${point.x},${point.y}`);
+	const connections = graph[`${point.x},${point.y}`];
+
+	for (let v of Object.keys(connections)) {
+		const [x, y] = v.split(",");
+		if (!seen.has(v))
+			max = Math.max(max, dfs({ x: +x, y: +y }, depth + 1) + connections[v]);
 	};
 
-	get isFinished() {
-		return this.pos.x === endColumn && (this.pos.y === (dataTxt.length - 1));
-	}
-}
+	seen.delete(`${point.x},${point.y}`);
 
-let concurrents = [new Solver(
-	{ x: startColumn, y: 0 },
-	[]
-)];
+	return max;
+};
 
-let lastPath: Solver | undefined;
-
-while (concurrents.length > 0) {
-	const pathsToMove = concurrents;
-	const newPaths: Solver[] = [];
-	pathsToMove.forEach(v => {
-		v.tryMove().forEach(w => {
-			if (w.isFinished) {
-				lastPath = w;
-			}
-			else
-				newPaths.push(w);
-		});
-	});
-	concurrents = newPaths;
-}
-
-console.log(lastPath?.path.length);
+console.log(dfs({ x: startColumn, y: 0 }, 0));
