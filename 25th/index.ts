10ee47e981be45960524968fadf276f6d9f32be1
drafolin <derg@drafolin.ch>
2023-12-27 00:18:14 +0100

    day 25 setup

---
+++ 25th/index.ts
@@ -0,0 +1,20 @@
+import { readFileSync } from "fs";
+
+const testing = false;
+const dataTxt = (testing ?
+	`jqt: rhn xhk nvd
+rsh: frs pzl lsr
+xhk: hfx
+cmg: qnr nvd lhk bvb
+rhn: xhk bvb hfx
+bvb: xhk hfx
+pzl: lsr hfx nvd
+qnr: nvd
+ntq: jqt hfx bvb xhk
+nvd: lhk
+lsr: lhk
+rzs: qnr cmg lsr rsh
+frs: qnr lhk lsr
+`: readFileSync(`${__dirname}/data.txt`, "utf-8"))
+	.trim()
+	.split("\n");
