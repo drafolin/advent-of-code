86440798b06a8c38d61240494ca91578a590d767
Dråfølin <derg@drafolin.ch>
2024-12-05 11:26:47 +0100

    2024 day 5 part 1

---
+++ 2024/5th/main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"errors"
+	"github.com/drafolin/advent-of-code/2024/utils"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+type pair [2]int
+
+func main() {
+	// data := "47|53\n97|13\n97|61\n97|47\n75|29\n61|13\n75|53\n29|13\n97|29\n53|29\n61|53\n97|53\n61|29\n47|13\n75|47\n97|75\n47|61\n75|61\n47|29\n75|13\n53|13\n\n75,47,61,53,29\n97,61,53,29,13\n75,29,13\n75,97,47,61,53\n61,13,29\n97,13,75,29,47"
+
+	data := utils.ReadInput("5th")
+
+	rules, updates, err := parseInput(data)
+	if err != nil {
+		panic(err)
+	}
+
+	sum := 0
+
+	for _, update := range updates {
+		valid := true
+		for _, rule := range rules {
+			if !slices.Contains(update, rule[0]) || !slices.Contains(update, rule[1]) {
+				continue
+			}
+
+			index1 := slices.Index(update, rule[0])
+			index2 := slices.Index(update, rule[1])
+
+			if index1 >= index2 {
+				valid = false
+				break
+			}
+		}
+
+		if valid {
+			middle, err := getMiddle(update)
+			if err != nil {
+				panic(err)
+			}
+			sum += middle
+		}
+	}
+
+	println(sum)
+}
+
+func parseInput(in string) ([]pair, [][]int, error) {
+	input := strings.Split(in, "\n\n")
+	rules := strings.Split(input[0], "\n")
+	updates := strings.Split(input[1], "\n")
+
+	parsedRules := make([]pair, len(rules))
+
+	for i, rule := range rules {
+		ruleSplit := strings.Split(rule, "|")
+		rule1, err := strconv.Atoi(ruleSplit[0])
+		if err != nil {
+			return nil, nil, err
+		}
+		rule2, err := strconv.Atoi(ruleSplit[1])
+		if err != nil {
+			return nil, nil, err
+		}
+		parsedRules[i] = pair{rule1, rule2}
+	}
+
+	parsedUpdates := make([][]int, len(updates))
+
+	for i, update := range updates {
+		updateSplit := strings.Split(update, ",")
+		parsedUpdates[i] = make([]int, len(updateSplit))
+		for j, num := range updateSplit {
+			var err error
+			parsedUpdates[i][j], err = strconv.Atoi(num)
+			if err != nil {
+				return nil, nil, err
+			}
+		}
+	}
+
+	return parsedRules, parsedUpdates, nil
+}
+
+func getMiddle(array []int) (int, error) {
+	if len(array)%2 == 0 {
+		return 0, errors.New("the array has an even number of elements")
+	}
+
+	return array[len(array)/2], nil
+}
