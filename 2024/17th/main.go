b670bbd687ad2067e0b987ebdc309a145f06c3b9
Dråfølin <derg@drafolin.ch>
2024-12-18 07:05:15 +0100

    2024 day 17 part 2

--- 2024/17th/main.go
+++ 2024/17th/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/drafolin/advent-of-code/2024/utils"
-	"math"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -12,101 +12,88 @@ func main() {
 	//data := strings.Split("Register A: 729\nRegister B: 0\nRegister C: 0\n\nProgram: 0,1,5,4,3,0", "\n\n")
 	data := strings.Split(utils.ReadInput("17th"), "\n\n")
 
-	registersStrs := strings.Split(data[0], "\n")
+	registerA := strings.Split(data[0], "\n")[0]
 	program := strings.Split(data[1], ",")
 	program[0] = strings.Split(program[0], " ")[1]
-
-	execPtr := 0
-	output := ""
-
-	registers := make(map[rune]int)
-	for _, reg := range registersStrs {
-		regSplit := strings.Split(reg, ": ")
-		regName := strings.Split(regSplit[0], " ")[1]
+	programUint := make([]uint64, len(program))
+	for i, v := range program {
 		var err error
-		registers[rune(regName[0])], err = strconv.Atoi(regSplit[1])
+		programUint[i], err = strconv.ParseUint(v, 10, 64)
 		if err != nil {
-			panic("Invalid register value")
+			panic(err)
 		}
 	}
 
-	for execPtr < len(program) {
-		opcode := program[execPtr]
-		operand := rune(program[execPtr+1][0])
+	seed, err := strconv.ParseUint(strings.Split(registerA, ": ")[1], 10, 64)
+	if err != nil {
+		panic("Invalid seed value")
+	}
+	fmt.Println(programUint, seed)
+	fmt.Println(partOne(programUint, seed))
+	fmt.Println(partTwo(programUint))
+}
 
-		switch opcode {
-		case "0":
-			// adv
-			numerator := registers['A']
-			denominator := math.Pow(2, float64(comboOperand(operand, registers)))
-			registers['A'] = numerator / int(denominator)
-		case "1":
-			// bxl
-			op1 := registers['B']
-			op2 := literalOperand(operand)
-			registers['B'] = op1 ^ op2
-		case "2":
-			//bst
-			registers['B'] = comboOperand(operand, registers) % 8
-		case "3":
-			// jnz
+func partTwo(program []uint64) (seed uint64) {
+	for itr := len(program) - 1; itr >= 0; itr-- {
+		seed <<= 3
+		for !slices.Equal(partOne(program, seed), program[itr:]) {
+			seed++
+		}
+	}
+
+	return
+}
+
+func partOne(program []uint64, seed uint64) (res []uint64) {
+	execPtr := 0
+
+	registers := map[rune]uint64{
+		'A': seed,
+		'B': 0,
+		'C': 0,
+	}
+
+	for execPtr < len(program)-1 {
+		operand := program[execPtr+1]
+
+		switch opcode := program[execPtr]; opcode {
+		case 0: // adv
+			registers['A'] >>= comboOperand(operand, registers)
+		case 1: // bxl
+			registers['B'] ^= operand
+		case 2: //bst
+			registers['B'] = comboOperand(operand, registers) & 7
+		case 3: // jnz
 			if registers['A'] != 0 {
-				execPtr = literalOperand(operand)
+				execPtr = int(operand)
 				continue
 			}
-		case "4":
-			//bxc
-			registers['B'] = registers['B'] ^ registers['C']
-		case "5":
-			// out
-			if output != "" {
-				output += ","
-			}
-			output += strconv.Itoa(comboOperand(operand, registers) % 8)
-		case "6":
-			// bdv
-			numerator := registers['A']
-			denominator := math.Pow(2, float64(comboOperand(operand, registers)))
-			registers['B'] = numerator / int(denominator)
-		case "7":
-			// cdv
-			numerator := registers['A']
-			denominator := math.Pow(2, float64(comboOperand(operand, registers)))
-			registers['C'] = numerator / int(denominator)
+		case 4: //bxc
+			registers['B'] ^= registers['C']
+		case 5: // out
+			res = append(res, comboOperand(operand, registers)&7)
+		case 6: // bdv
+			registers['B'] = registers['A'] >> comboOperand(operand, registers)
+		case 7: // cdv
+			registers['C'] = registers['A'] >> comboOperand(operand, registers)
 		}
 
 		execPtr += 2
 	}
-
-	fmt.Println(output)
+	return
 }
 
-func comboOperand(operand rune, registers map[rune]int) int {
+func comboOperand(operand uint64, registers map[rune]uint64) uint64 {
 	switch operand {
-	case '0':
-		return 0
-	case '1':
-		return 1
-	case '2':
-		return 2
-	case '3':
-		return 3
-	case '4':
+	case 0, 1, 2, 3:
+		return operand
+	case 4:
 		return registers['A']
-	case '5':
+	case 5:
 		return registers['B']
-	case '6':
+	case 6:
 		return registers['C']
-	case '7':
+	default:
 		panic("Reserved operand")
 	}
-	return 0
-}
-
-func literalOperand(operand rune) int {
-	i, err := strconv.Atoi(string(operand))
-	if err != nil {
-		panic("Invalid operand")
-	}
-	return i
 }
